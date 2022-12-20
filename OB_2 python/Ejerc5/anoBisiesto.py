ab2367c934c55ae1e03897cd256b3921617be2d1
Tomás Canavidez <tms1csgo@gmail.com>
2022-12-20 20:08:12 -0300

    Ejercicio 5 año bisiesto

---
+++ OB_2 python/Ejerc5/anoBisiesto.py
@@ -0,0 +1,12 @@
+año = int(input("Ingrese un año: "))
+
+if año % 4 != 0:
+    print("NO es un año BISIESTO")
+elif año % 4 == 0 and año % 100 != 0:
+    print("Es un año BISIESTO")
+elif año % 4 == 0 and año % 100 == 0 and año % 400 != 0:
+    print("NO es un año BISIESTO")
+elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0:
+    print("Es un año BISIESTO")
+
+    
