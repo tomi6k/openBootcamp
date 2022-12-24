95864e8b281a35e962ed7c4aee211777e1a83df0
Tomás Canavidez <tms1csgo@gmail.com>
2022-12-24 15:04:16 -0300

    Update herenciaVehiculoCoche.py

--- OB_2 python/Ejerc6.1/herenciaVehiculoCoche.py
+++ OB_2 python/Ejerc6.1/herenciaVehiculoCoche.py
@@ -1,15 +1,23 @@
 class Vehiculo():
-    Color = "Rojo"
-    Ruedas = "4"
-    Puertas = "4"
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.puertas = puertas
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return "color {}, ruedas {}, puertas {}".format(self.color, self.ruedas, self.puertas)
 
 class Coche(Vehiculo):
-    Velocidad = 300
-    Cilindrada = "1300cc"
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        super().__init__(color, ruedas, puertas,)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
-p = Coche()
+    def __str__(self):
+        return Vehiculo.__str__(self) + '{} km/h, {} cc' .format(self.cilindrada, self.velocidad)
 
-print("El coche tiene: ", p.Ruedas, " ruedas.")
-print("Tambien tiene " , p.Puertas, " puertas")
-print("Su color es: ", p.Color)
-print("Su velocidad maxima es ",p.Velocidad, " con ", p.Cilindrada, " en su motor.")
+ford = Coche('rojo', 4,5,190,1200)
+volkswagen = Coche('gris',4,3,220,1600)
+
+print(ford)
+print(volkswagen)
